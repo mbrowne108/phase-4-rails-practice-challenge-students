56480cb84b0ef7b266aba5099bddb4d82a8bcaf7
Matt <mbrowne108@gmail.com>
2022-03-01 14:51:39 -0500

    Add routes and controllers

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,48 @@
+class InstructorsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        render json: Instructor.all
+    end
+
+    def show
+        instructor = find_instructor
+        render json: instructor
+    end
+
+    def destroy
+        instructor = find_instructor
+        instructor.destroy
+        head :no_content
+    end
+
+    def update
+        instructor = find_instructor
+        instructor.update(instructor_params)
+        render json: instructor
+    end
+
+    def create
+        instructor.create!(instructor_params)
+        render json: instructor, status: :created
+    end
+
+    private
+    
+    def find_instructor
+        Instructor.find(params[:id])
+    end
+
+    def instructor_params
+        params.permit(:id, :name)    
+    end
+
+    def render_not_found_response
+        render json: { error: "Instructor not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response (exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
